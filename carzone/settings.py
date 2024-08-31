ebf734017fb14df2c5435d3700b7059775b5f95a
Asus_Abhishek <dubeyabhi065@gmail.com>
2024-08-31 13:49:35 +0530

    edit

--- carzone/settings.py
+++ carzone/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'whitenoise.runserver_nostatic',
+    
 
     # Providers
    'allauth.socialaccount.providers.facebook',
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    ###'whitenoise.middleware.WhiteNoiseMiddleware',
     
 ]
 
